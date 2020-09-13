6bf412a36003f146cf96f751a7baf8cd3a485d54
ashish <ashishmaju@gmail.com>
2020-09-13 23:51:59 +0530

    Path change

--- Framework/src/main/java/com/ndtv/propertiesFile/PropertyFile.java
+++ Framework/src/main/java/com/ndtv/propertiesFile/PropertyFile.java
@@ -9,9 +9,8 @@ public class PropertyFile {
 	public static String propKey(String key) {
 		prop= new Properties();
 		try {
-
 			FileInputStream fis = new FileInputStream(
-					System.getProperty("user.dir") + "/src/main\\java\\com\\ndtv\\propertiesFile\\Data.properties");
+					System.getProperty("user.dir") + "\\src\\main\\java\\com\\ndtv\\propertiesFile\\Data.properties");
 			prop.load(fis);
 			return prop.getProperty(key);
 		} catch (IOException e) {
